8a455c9b05f20acdda76a6488f428b30b94b291d
Timo Vandrey <timo.vandrey@haw-hamburg.de>
2021-06-06 10:55:02 +0200

    Add project files.

---
+++ MainForm.Designer.cs
@@ -0,0 +1,115 @@
+﻿
+namespace MinimalDiary
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.MainMenu = new System.Windows.Forms.MenuStrip();
+            this.MainMenuItemSave = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenuItemSetDefaultLocation = new System.Windows.Forms.ToolStripMenuItem();
+            this.MainMenuDefaultSaveLocation = new System.Windows.Forms.ToolStripTextBox();
+            this.MainTextBox = new System.Windows.Forms.RichTextBox();
+            this.MainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // MainMenu
+            // 
+            this.MainMenu.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.MainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.MainMenuItemSave,
+            this.MainMenuItemSetDefaultLocation,
+            this.MainMenuDefaultSaveLocation});
+            this.MainMenu.Location = new System.Drawing.Point(0, 0);
+            this.MainMenu.Name = "MainMenu";
+            this.MainMenu.Size = new System.Drawing.Size(800, 35);
+            this.MainMenu.TabIndex = 0;
+            this.MainMenu.Text = "MainMenu";
+            //this.MainMenu.ItemClicked += new System.Windows.Forms.ToolStripItemClickedEventHandler(this.MainMenu_ItemClicked);
+            // 
+            // MainMenuItemSave
+            // 
+            this.MainMenuItemSave.Name = "MainMenuItemSave";
+            this.MainMenuItemSave.Size = new System.Drawing.Size(65, 31);
+            this.MainMenuItemSave.Text = "Save";
+            this.MainMenuItemSave.ToolTipText = "Save your diary file (to default location when set, otherwise somewhere else).";
+            // 
+            // MainMenuItemSetDefaultLocation
+            // 
+            this.MainMenuItemSetDefaultLocation.Name = "MainMenuItemSetDefaultLocation";
+            this.MainMenuItemSetDefaultLocation.Size = new System.Drawing.Size(53, 31);
+            this.MainMenuItemSetDefaultLocation.Text = "Set";
+            this.MainMenuItemSetDefaultLocation.ToolTipText = "Sets the default (directory) location entered on the right text box and saves it." +
+    "";
+            // 
+            // MainMenuDefaultSaveLocation
+            // 
+            this.MainMenuDefaultSaveLocation.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.MainMenuDefaultSaveLocation.AutoSize = false;
+            this.MainMenuDefaultSaveLocation.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MainMenuDefaultSaveLocation.Name = "MainMenuDefaultSaveLocation";
+            this.MainMenuDefaultSaveLocation.Size = new System.Drawing.Size(100, 31);
+            this.MainMenuDefaultSaveLocation.ToolTipText = "The default (directory) location where your diary will be saved.";
+            // 
+            // MainTextBox
+            // 
+            this.MainTextBox.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.MainTextBox.Location = new System.Drawing.Point(0, 35);
+            this.MainTextBox.Name = "MainTextBox";
+            this.MainTextBox.Size = new System.Drawing.Size(800, 415);
+            this.MainTextBox.TabIndex = 1;
+            this.MainTextBox.Text = "";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.MainTextBox);
+            this.Controls.Add(this.MainMenu);
+            this.MainMenuStrip = this.MainMenu;
+            this.Name = "MainForm";
+            this.Text = "Minimal Diary";
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            
+            this.MainMenu.ResumeLayout(false);
+            this.MainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip MainMenu;
+        private System.Windows.Forms.ToolStripMenuItem MainMenuItemSave;
+        private System.Windows.Forms.ToolStripTextBox MainMenuDefaultSaveLocation;
+        private System.Windows.Forms.RichTextBox MainTextBox;
+        private System.Windows.Forms.ToolStripMenuItem MainMenuItemSetDefaultLocation;
+    }
+}
+
---
+++ MainForm.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Security;
+
+namespace MinimalDiary
+{
+    public partial class MainForm : Form
+    {
+        const string ProgramName = "Minimal Diary";
+
+        public MainForm()
+        {
+            InitializeComponent();
+            this.Resize += new System.EventHandler(this.MainForm_Resize);
+            this.MainMenuItemSave.Click += MainMenuItemSave_Click;
+            this.MainMenuItemSetDefaultLocation.Click += MainMenuItemSetDefaultLocation_Click;
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            ResizeMainMenuTextBox();
+            LoadDefaultLocation();
+        }
+
+        private void MainForm_Resize(object sender, EventArgs e)
+        {
+            ResizeMainMenuTextBox();
+        }
+
+        private void ResizeMainMenuTextBox()
+        {
+            // Resize TextBoxDefaultSaveLocation to fit entire MainMenuBar
+            var newsize = MainMenu.Size.Width - (MainMenuItemSetDefaultLocation.Size.Width + MainMenuItemSave.Size.Width + 20);
+            MainMenuDefaultSaveLocation.Size = new Size(newsize, MainMenuDefaultSaveLocation.Size.Height);
+        }
+
+        private void LoadDefaultLocation()
+        {
+            // TODO: Create this
+            try
+            {
+
+            }
+            catch (Exception ex)
+            {
+
+                throw;
+            }
+        }
+
+        private void MainMenuItemSetDefaultLocation_Click(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void MainMenuItemSave_Click(object sender, System.EventArgs e)
+        {
+            bool DefaultWasEnabled = false;
+            string Name = string.Join("-", ProgramName.Split(" ")) +
+                "_" +
+                DateTime.Now.ToString("yyyy-MM-dd-HH-mm-ss");
+            string SaveLocationPath = String.Empty;
+            if (string.IsNullOrEmpty(MainMenuDefaultSaveLocation.Text))
+            {
+                // Save as ...
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Text files (*.txt)|*.txt";
+                sfd.FileName = Name;
+                sfd.Title = "Save file ...";
+                sfd.CheckPathExists = true;
+                var res = sfd.ShowDialog();
+                if (res != DialogResult.OK || sfd.FileNames.Length > 1)
+                {
+                    return;
+                }
+
+                SaveLocationPath = sfd.FileName;
+                DefaultWasEnabled = false;
+            }
+            else
+            {
+                // Save in default location
+                SaveLocationPath = MainMenuDefaultSaveLocation.Text;
+                DefaultWasEnabled = true;
+            }
+
+            if (!Directory.Exists(Path.GetDirectoryName(SaveLocationPath)))
+            {
+                MessageBox.Show("Directory doesn't exist!", 
+                    "Error!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            string SaveLocationAndName = String.Empty;
+            if (DefaultWasEnabled)
+            {
+                SaveLocationAndName = Path.Combine(SaveLocationPath, Name) + ".txt";
+            }
+            else
+            {
+                SaveLocationAndName = SaveLocationPath;
+            }
+            try
+            {
+                using (FileStream stream = new FileStream(SaveLocationAndName, FileMode.Create))
+                {
+                    using (StreamWriter writer = new StreamWriter(stream))
+                    {
+                        writer.Write(MainTextBox.Text);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Something went wrong when trying to save your file!\nMessage:\n{ex.Message}",
+                    "Error!",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Error);
+                return;
+            }
+
+            var res2 = MessageBox.Show("Saved successfully. Exit now?",
+                "Success!",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Information);
+            if(res2 == DialogResult.Yes)
+            {
+                Environment.Exit(0);
+            }
+        }
+
+    }
+}
